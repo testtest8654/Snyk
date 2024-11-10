14e008aefc3a0bb1d0ca873cfad04ae4273be777
testtest8654 <159459961+testtest8654@users.noreply.github.com>
2024-11-10 15:37:39 +0530

    Delete web_pod_diagnostics directory

--- web_pod_diagnostics/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DOCKER_HOST="ssh://root@178.62.64.69"
-docker build --tag=web_pod_diagnostics .
-docker run -it -p 1337:80 --rm --name=web_pod_diagnostics web_pod_diagnostics
-
--- web_pod_diagnostics/challenge/web/filters.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def format_date(date, *, format_str="%Y-%m-%d"):
-    if not isinstance(date, datetime):
-        date = datetime.strptime(date, '%Y-%m-%d')
-    return date.strftime(format_str)
