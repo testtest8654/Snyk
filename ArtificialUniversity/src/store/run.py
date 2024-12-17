775b844a42f2eb2e3f0fc840efe3a235b694ad08
testtest8654 <159459961+testtest8654@users.noreply.github.com>
2024-12-17 13:01:30 +0530

    Delete ArtificialUniversity directory

--- ArtificialUniversity/src/store/run.py
+++
@@ -1,8 +0,0 @@
-from application.app import app
-from application.util.database import Database
-
-db_session = Database()
-db_session.migrate(app.config["ADMIN_EMAIL"], app.config["ADMIN_PASS"])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1337, threaded=True, debug=False)
