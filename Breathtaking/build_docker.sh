037e4837e7ee6785b47e86cf9d7f6ce9d86f01c6
testtest8654 <159459961+testtest8654@users.noreply.github.com>
2025-01-20 13:11:10 +0530

    Delete Breathtaking directory

--- Breathtaking/build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker rm -f web_breahtaking_view
-docker build --no-cache --tag=web_breahtaking_view .
-docker run -p 1337:8081 --rm --name=web_breahtaking_view -it web_breahtaking_view
--- Breathtaking/src/main/java/com/hackthebox/breathtaking_view/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hackthebox.breathtaking_view.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hackthebox.breathtaking_view.Models.Users;
-public interface UserRepository extends JpaRepository<Users, Long> {
-    Users findByUsername(String username);
-}
