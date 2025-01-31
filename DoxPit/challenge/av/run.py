42593839ccf5faf4551ad45236931b8f92d1a78a
testtest8654 <159459961+testtest8654@users.noreply.github.com>
2025-01-31 13:29:52 +0530

    Delete DoxPit directory

--- DoxPit/challenge/av/run.py
+++
@@ -1,6 +0,0 @@
-from application.app import app
-from application.util.database import Database
-
-if __name__ == "__main__":
-    Database().migrate()
-    app.run(host="0.0.0.0", port=3000, threaded=True, debug=False)
