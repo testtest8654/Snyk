ebee33bc69f5b6463a69ea2ead9508091d5f4bad
testtest8654 <159459961+testtest8654@users.noreply.github.com>
2025-02-04 17:30:54 +0530

    Delete Tornado directory

--- Tornado/build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker rm -f web_tornado_service
-docker build --tag=web_tornado_service .
-docker run -p 1337:1337 --rm --name=web_tornado_service -it web_tornado_service
--- Tornado/challenge/run.py
+++
@@ -1,7 +0,0 @@
-import tornado.ioloop
-
-from application.main import APP
-
-if __name__ == "__main__":
-    APP.listen(1337, address="0.0.0.0")
-    tornado.ioloop.IOLoop.current().start()
